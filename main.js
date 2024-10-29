050ad228dc8668f43e139fd93d774f0bccd1275c
Anadurso <160138997+Anadurso@users.noreply.github.com>
2024-10-29 08:54:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'teste'
+    article.innerHTML = `
+        <div class="cartao-conteudo">
+            <h3> ${categoria} </h3>
+            <div class="cartao-conteudo-pergunta">
+                <p> ${pergunta} </p>
+            </div>
+            <div class="cartao-conteudo-resposta">
+                <p> ${resposta} </p>
+            </div>
+        </div>
+    `
     container.appendChild(article)
 }
 
 criaCartao('Biologia','Qual é a função da mitocondria?','Produção de Energia (ATP)')
+criaCartao('Matematica','Como é definido um grupo?','É um conjunto unido de uma operação onde esta é fechada no conjunto e tem algumas prioridades')
